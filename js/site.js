b033defc751e4fdb70ce3698f464d877b7e744b0
HLOIS MLO <HLOIS.MLO@gmail.com>
2024-07-17 12:42:02 -0400

    codechanges in sitesjs

--- js/site.js
+++ js/site.js
@@ -1,14 +1,13 @@
 //get values from the screen and display them
 function getValues(){
    let PalindromeMessage = document.getElementById("message").value;
-
+   PalindromeMessage = PalindromeMessage.toLowerCase() .replace(/\s/g,"");
    let reverseMessage = reverse(PalindromeMessage);
    let isPalindrome = checkForPalindrome(PalindromeMessage, reverseMessage);
    //convert palindrome message to lowercase then reverse it then compare to see if same
-   NotaPalindromeMessage.innerHTML = displayResults(PalindromeMessage, reverseMessage, isPalindrome);
+   displayResults(PalindromeMessage, reverseMessage, isPalindrome);
 }
-
-
+//Check for Palindrome
 function checkForPalindrome(message, reversemessage){
    if(message == reversemessage){
     return true;
@@ -31,11 +30,28 @@ function reverse(message){
 
      
   function displayResults(PalindromeMessage, reverseMessage, isPalindrome){  
+   //This retrieves the alert element
     let alertElement = document.getElementById("alertMessage");
+    let message= "";
+   if(isPalindrome){
+      alertElement.classList.add("alert-success");
+      alertElement.classList.remove("alert-danger");
+          message = 
+                  `<div>
+                  You've entered :<strong> ${PalindromeMessage} </strong> 
+                  Your phrase reversed is:<strong> ${reverseMessage},</strong>
+                  your phrase is a palindrone :<strong>${isPalindrome},</strong>.</div>`;
+   }else{
+      alertElement.classList.remove("alert-success");
+      alertElement.classList.add("alert-danger");
+          message = 
+                  `<div>
+                  You've entered :<strong> ${PalindromeMessage} </strong> 
+                  Your phrase reversed is:<strong> ${reverseMessage},</strong>
+                  your phrase is not a palindrone :<strong>${isPalindrome},</strong>.</div>`;   
+   }
 
     alertElement.style.opacity = 1;  
-    
-    
-    let message = `<div>Your phrase reversed is:<strong> ${reverseMessage}, your phrase is a palindrone :<strong>${isPalindrome}, your phrase is a:<strong> ${PalindromeMessage}.</strong>. </div>`;
+
  alertElement.innerHTML = message
 }
