8824e84ce58e848f57e772e731b169cdf2d6753e
HLOIS MLO <HLOIS.MLO@gmail.com>
2024-09-08 22:54:11 -0400

    updated footer and code page

--- js/site.js
+++ js/site.js
@@ -1,57 +1,57 @@
 //get values from the screen and display them
-function getValues(){
+function getValues() {
    let PalindromeMessage = document.getElementById("message").value;
-   PalindromeMessage = PalindromeMessage.toLowerCase() .replace(/\s/g,"");
+   PalindromeMessage = PalindromeMessage.toLowerCase().replace(/\s/g, "");
    let reverseMessage = reverse(PalindromeMessage);
    let isPalindrome = checkForPalindrome(PalindromeMessage, reverseMessage);
    //convert palindrome message to lowercase then reverse it then compare to see if same
    displayResults(PalindromeMessage, reverseMessage, isPalindrome);
 }
-//Check for Palindrome
-function checkForPalindrome(message, reversemessage){
-   if(message == reversemessage){
-    return true;
-   } else{
-    return false;
+//check for Palindrome
+function checkForPalindrome(message, reversemessage) {
+   if (message == reversemessage) {
+      return true;
+   } else {
+      return false;
 
    }
 }
 
 //return the message in reverse (Pallindrome)
-function reverse(message){
-      let reverseMessage = "";
-      for(let index = message.length-1; index>=0; index--){
-         reverseMessage += message[index]; 
-      }
+function reverse(message) {
+   let reverseMessage = "";
+   for (let index = message.length - 1; index >= 0; index--) {
+      reverseMessage += message[index];
+   }
 
-      return reverseMessage;
+   return reverseMessage;
 }
 
 
-     
-  function displayResults(PalindromeMessage, reverseMessage, isPalindrome){  
+//display the results     
+function displayResults(PalindromeMessage, reverseMessage, isPalindrome) {
    //This retrieves the alert element
-    let alertElement = document.getElementById("alertMessage");
-    let message= "";
-   if(isPalindrome){
+   let alertElement = document.getElementById("alertMessage");
+   let message = "";
+   if (isPalindrome) {
       alertElement.classList.add("alert-success");
       alertElement.classList.remove("alert-danger");
-          message = 
-                  `<div>
+      message =
+         `<div>
                   You've entered :<strong> ${PalindromeMessage} </strong> 
                   Your phrase reversed is:<strong> ${reverseMessage},</strong>
                   your phrase is a palindrone :<strong>${isPalindrome},</strong>.</div>`;
-   }else{
+   } else {
       alertElement.classList.remove("alert-success");
       alertElement.classList.add("alert-danger");
-          message = 
-                  `<div>
+      message =
+         `<div>
                   You've entered :<strong> ${PalindromeMessage} </strong> 
                   Your phrase reversed is:<strong> ${reverseMessage},</strong>
-                  your phrase is not a palindrone :<strong>${isPalindrome},</strong>.</div>`;   
+                  your phrase is not a palindrone :<strong>${isPalindrome},</strong>.</div>`;
    }
 
-    alertElement.style.opacity = 1;  
+   alertElement.style.opacity = 1;
 
- alertElement.innerHTML = message
+   alertElement.innerHTML = message
 }
